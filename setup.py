53d42aba7990b15f56a1a549d96fa48ccedf4014
Arthur Juliani <awjuliani@gmail.com>
2019-02-07 14:42:46 -0800

    Require new standalone package

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
-from os import path
+from setuptools import setup
 
 setup(
     name='obstacle_tower_env',
     version='0.1',
     py_modules=["obstacle_tower_env"],
-    install_requires=['mlagents==0.6', 'gym-unity==0.1.1', 'Pillow==5.2.0'],
+    install_requires=['mlagents-envs==0.6']
 )
