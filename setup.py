2fa7c442a882a97697e2788d60037b8b7ea3df2b
Arthur Juliani <awjuliani@gmail.com>
2019-02-07 15:12:04 -0800

    Updates to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='obstacle_tower_env',
-    version='0.1',
+    version='0.2',
+    author='Unity Technologies',
+    url='https://github.com/Unity-Technologies/obstacle-tower-env',
     py_modules=["obstacle_tower_env"],
-    install_requires=['mlagents-envs==0.6']
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['mlagents-envs>=0.6,<0.7']
 )
