6869a88f51a00cb69132b36c152ec8bc5fc4612b
Arthur Juliani <awjuliani@gmail.com>
2019-06-10 15:02:15 -0400

    Target 2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='obstacle_tower_env',
-    version='2.1',
+    version='2.2',
     author='Unity Technologies',
     url='https://github.com/Unity-Technologies/obstacle-tower-env',
     py_modules=["obstacle_tower_env"],
