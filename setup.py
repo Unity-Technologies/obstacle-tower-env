e2efa3910c12317da15c683b6d6ed7156cdbf1d1
Arthur Juliani <awjuliani@gmail.com>
2019-10-08 13:32:40 -0400

    Update env for v3.1

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name='obstacle_tower_env',
-    version='3.0',
+    version='3.1',
     author='Unity Technologies',
     url='https://github.com/Unity-Technologies/obstacle-tower-env',
     py_modules=["obstacle_tower_env"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['mlagents-envs>=0.8.1,<0.9',
+    install_requires=['mlagents-envs>=0.10,<0.11',
                       'gym']
 )
