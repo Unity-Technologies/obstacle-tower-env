249a60adcbe58e1a7ec07781a82b202940dae0fa
Arthur Juliani <awjuliani@gmail.com>
2020-06-25 16:13:36 -0700

    Refactor to work with latest ML-Agents API

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='obstacle_tower_env',
-    version='3.1',
-    author='Unity Technologies',
-    url='https://github.com/Unity-Technologies/obstacle-tower-env',
+    name="obstacle_tower_env",
+    version="4.0",
+    author="Unity Technologies",
+    url="https://github.com/Unity-Technologies/obstacle-tower-env",
     py_modules=["obstacle_tower_env"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['mlagents-envs>=0.10,<0.11',
-                      'gym']
+    install_requires=["mlagents-envs>=0.16,<0.17", "gym"],
 )
